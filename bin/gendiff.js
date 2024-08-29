269c8f1af92ac88a2db1d9676db63faa6cb45c15
Serhio Bon <bssbss163@gmail.com>
2024-08-29 18:09:59 +0400

    modified  bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import { program } from 'commander';
 
 const gendiff = () => {
     program
-        .version('0.0.1', '-v, --version', 'output the version number')
-        .description('Compares two configuration files and shows a difference.');
+        .version('0.0.1', '-V, --version', 'output the version number')
+        .description('Compares two configuration files and shows a difference.')
+        .arguments('<filepath1> <filepath2>')
+        .option('-f, --format [type]', 'output format')
     program.parse(process.argv);
 };
 
