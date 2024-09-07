379ff08136744b8e91fba45f874818a56f50fd1e
Serhio Bon <bssbss163@gmail.com>
2024-09-07 21:36:31 +0400

    add new file getdiff.js

---
+++ src/getdiff.js
@@ -0,0 +1,26 @@
+import getParse from './parsers.js'
+import _ from 'lodash';
+
+const getDiff = (filepath1, filepath2) => {
+    const file1Data = getParse(filepath1);
+    const file2Data = getParse(filepath2);
+    
+    const file1Keys = Object.keys(file1Data);
+    const file2Keys = Object.keys(file2Data);
+    const allUniqKeys = _.sortBy(_.union(file1Keys, file2Keys));
+    
+    const diff = allUniqKeys.map((key) => {
+        if (_.has(file1Data, key) && _.has(file2Data, key)) {
+            if (file1Data[key] === file2Data[key]) {
+                return `  ${key}: ${file1Data[key]}`;
+            } else 
+                return [`- ${key}: ${file1Data[key]}`, `+ ${key}: ${file2Data[key]}`];
+        } if (_.has(file1Data, key) && !(_.has(file2Data, key))) {
+            return `- ${key}: ${file1Data[key]}`;
+        } return `+ ${key}: ${file2Data[key]}`;
+    });
+    const result = diff.flat();
+    return `{\n  ${result.join('\n  ')} \n}`;
+};
+
+export default getDiff;
