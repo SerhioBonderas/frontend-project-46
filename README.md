38c5c6ead64cb65c4354e27f50ed2299ea704270
Serhio Bon <bssbss163@gmail.com>
2024-09-09 21:35:25 +0400

    step 6 diff yml

--- README.md
+++ README.md
@@ -8,5 +8,8 @@
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/bde003dfc63f5ea9d37a/test_coverage)](https://codeclimate.com/github/SerhioBonderas/frontend-project-46/test_coverage)
 
-### Demonstration of work after step 4:
+### Demonstration of work with .json files:
 https://asciinema.org/a/oVkyphc1jtntGq9QIXDhAKyce
+
+### Demonstration of work with .yml files:
+ https://asciinema.org/a/D5xLsgu34OaoBbebFwQYVNF4I
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,8 +6,11 @@ import genDiff from '../src/index.js';
 test('genDiff', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
+  const path3 = getFixturePath('file1.yml');
+  const path4 = getFixturePath('file2.yml');
 
-  const data = fs.readFileSync(getFixturePath('deff-result.txt'), 'utf-8');
+  const data = fs.readFileSync(getFixturePath('test-result.txt'), 'utf-8');
 
-  expect(genDiff(path1, path2)).toEqual(data);
+  expect(genDiff(path1, path2)).toBe(data);
+  expect(genDiff(path3, path4)).toBe(data);
 });
